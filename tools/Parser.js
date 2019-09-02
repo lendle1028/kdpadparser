32f10d28764a364ac470ae67ebbf919c6fe89ec3
lendle <lendle_tseng@seed.net.tw>
2019-09-02 16:35:42 +0800

    implementing the parser

--- tools/Parser.js
+++ tools/Parser.js
@@ -1,16 +1,50 @@
 class Parser{
-    static parse(codeStringList){
-        let codeContainerStack=new Array();
-        let blockStack=new Array();
-        this.codeStringList=codeStringList;
+    constructor(){
+        this.codeContainerStack=new Array();
+        this.blockStack=new Array();
     }
 
     /**
-     * @returns a list of expressions in this block
+     * @returns a list of top-level expression instance
+     */
+    parse(codeString){
+        let codeStringList=codeString.split(/\n/);
+
+    }
+
+    parseCodeTree(codeLines, rootNode){
+        let currentLine=codeLines[0];
+        let node=new ParserTreeNode(currentLine);
+        rootNode.getChildNodes().push(node);
+        let level=this.countPrefixWhiteSpaces(currentLine);
+        codeLines.splice(0, 1);
+        while(codeLines.length>0){
+            let nextLine=codeLines[0];
+            let nextLevel=this.countPrefixWhiteSpaces(nextLine);
+            if(nextLevel>level){
+                //go to the next level
+                let childNode=this.parseCodeTree(codeLines, node);
+                childNode.setParentNode(node);
+                node.getChildNodes().push(childNode);
+                codeLines.splice(0, 1);//consume the node
+            }else if(nextLevel==level){
+                let childNode=new ParserTreeNode(nextLine);
+                rootNode.getChildNodes().push(childNode);
+                codeLines.splice(0, 1);//consume the node
+            }else{
+                //this is the end of this block
+                break;
+            }
+        }
+        return node;
+    }
+
+    /**
+     * @returns an expression instance
      * @param {*} currentCodeString 
      * @param {*} remainingCodeString 
      */
-    static _parse(currentCodeString, remainingCodeString){
+    _parse(currentCodeString, remainingCodeString){
         let exp=null;//parse currentCodeString to an expression
         let nextLine=remainingCodeString[0];
         if(Parser.countPrefixWhiteSpaces(currentCodeString)<Parser.countPrefixWhiteSpaces(nextLine)){
@@ -19,7 +53,7 @@ class Parser{
         }
     }
 
-    static countPrefixWhiteSpaces(line){
+    countPrefixWhiteSpaces(line){
         let count=0;
         for(let c of line){
             if(c==' '){
@@ -29,5 +63,33 @@ class Parser{
             }
         }
         return count;
+    }
+}
+
+/**
+ * a tree to hold the source codes
+ * each node is a line of code
+ */
+class ParserTreeNode{
+    constructor(line){
+        this.parentNode=null;
+        this.childNodes=[];
+        this.line=null;
+    }
+
+    getLine(){
+        return this.line;
+    }
+
+    getParentNode(){
+        return this.parentNode;
+    }
+
+    setParentNode(node){
+        this.parentNode=node;
+    }
+
+    getChildNodes(){
+        return this.childNodes;
     }
 }
